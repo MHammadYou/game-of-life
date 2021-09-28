04a6a150e7402e7c33ada75a403764118efd9d6b
MHammadYou <MHammadY2.0@gmail.com>
2021-09-28 22:43:45 +0500

    Added basic Cell and World classes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,72 @@
 #include <iostream>
 
-
-class World
+class Cell
 {
+private:
+  std::string m_State;
+  int m_X, m_Y;
 
+public:
+  Cell(const std::string& state, int x, int y)
+  {
+    m_State = state;
+    m_X = x;
+    m_Y = y;
+  }
+
+  std::string getState()
+  {
+    return m_State;
+  }
+
+  int getX() const
+  {
+    return m_X;
+  }
+
+  int getY() const
+  {
+    return m_Y;
+  }
 };
 
 
-class Cell
+class World
+{
+private:
+  Cell* m_World[10][10];
+
+public:
+  World() {
+
+    for (int i = 0; i < 10; i++)
+    {
+      for (int j = 0; j < 10; j++)
+      {
+        m_World[i][j] = new Cell(".", i, j);
+      }
+    }
+  }
+  void logWorld()
+  {
+    for (int i = 0; i < 10; i++)
+    {
+      std::cout << "_";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < 10; i++)
+    {
+      for (int j = 0; j < 10; j++)
+      {
+        std::cout << m_World[i][j]->getState();
+      }
+      std::cout << std::endl;
+    }
+  }
+};
+
+
+class Rules
 {
 
 };
@@ -16,5 +75,7 @@ class Cell
 
 int main()
 {
+  World world;
+  world.logWorld();
 
 }
