46898ca6ff6fbc150fbf923cc8560732a704bea2
MHammadYou <MHammadY2.0@gmail.com>
2021-09-29 22:48:41 +0500

    Updated Cell class

--- main.cpp
+++ main.cpp
@@ -14,6 +14,19 @@ public:
     m_Y = y;
   }
 
+  void getNeighbours(Cell& world)
+  {
+    int neighbours[8][2] = {
+      {m_X - 1, m_Y - 1}, {m_X - 1, m_Y}, {m_X - 1, m_Y + 1},
+      {m_X, m_Y - 1},                     {m_X, m_Y + 1},
+      {m_X + 1, m_Y - 1}, {m_X + 1, m_Y}, {m_X + 1, m_Y + 1},};
+
+    for (int i = 0; i < 8; i++)
+    {
+      std::cout << "X: " << neighbours[i][0] << "  Y: " << neighbours[i][1] << std::endl;
+    }
+  }
+
   std::string getState()
   {
     return m_State;
@@ -37,30 +50,43 @@ private:
   Cell* m_World[10][10];
 
 public:
-  World() {
+  World() : m_World() {
 
     for (int i = 0; i < 10; i++)
     {
       for (int j = 0; j < 10; j++)
       {
-        m_World[i][j] = new Cell(".", i, j);
+        m_World[i][j] = new Cell(" . ", i, j);
       }
     }
   }
+
+  void doSomething()
+  {
+//    m_World[2][2]->getNeighbours(&m_World);
+  }
+
   void logWorld()
   {
+    std::cout << "  ";
     for (int i = 0; i < 10; i++)
     {
-      std::cout << "_";
+      std::cout << "___";
     }
     std::cout << std::endl;
     for (int i = 0; i < 10; i++)
     {
+      std::cout << "| ";
       for (int j = 0; j < 10; j++)
       {
         std::cout << m_World[i][j]->getState();
       }
-      std::cout << std::endl;
+      std::cout << " |" << std::endl;
+    }
+    std::cout << "  ";
+    for (int i = 0; i < 10; i++)
+    {
+      std::cout << "___";
     }
   }
 };
